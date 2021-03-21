266cc8fe4d41f79a02fdf9379858d8bb4abaa29c
Mikhail Kirpichov <mishania261282@mail.ru>
2021-03-21 22:30:41 +0300

    refactor: updated index.html style.css script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ keys.forEach(function (key) { key.addEventListener('click', playAudio); });
 window.addEventListener('keydown', playSound);
 window.addEventListener('keydown', (event) => playSound(event));
 
+const buttonLetters = document.querySelectorAll('.button-letters');
+const buttonNotes = document.querySelectorAll('.button-notes');
+buttonLetters.forEach(function (button) { button.addEventListener('click', renameLetters); });
+buttonNotes.forEach(function (button) { button.addEventListener('click', renameNotes); });
+
 function playAudio(e) {
   let key = e.target;
   let note = document.getElementById(key.dataset.note);
@@ -23,4 +28,55 @@ function playSound(e) {
   audio.play();
   audio.addEventListener('ended',() =>{key.classList.remove('active');})
 
+}
+function renameLetters(e) {
+  let button = e.target;
+  button.classList.add('active');
+  let elem = document.querySelector("#a");
+  elem.classList.remove('button-active');
+  elem.classList.remove('active');
+  
+  let d = document.getElementById('d');
+  let f = document.getElementById('f');
+  let g = document.getElementById('g');
+  let h = document.getElementById('h');
+  let j = document.getElementById('j');
+  let k = document.getElementById('k');
+  let l = document.getElementById('l');
+  let r = document.getElementById('r');
+  let t = document.getElementById('t');
+  let u = document.getElementById('u');
+  let i = document.getElementById('i');
+  let o = document.getElementById('o');
+
+
+  d.innerText = "D";f.innerText = "F";g.innerText = "G";h.innerText = "H";j.innerText = "J";k.innerText = "K";
+  l.innerText = "L";r.innerText = "R";t.innerText = "T";u.innerText = "U";i.innerText = "I";o.innerText = "O";
+  
+  
+}
+function renameNotes(e) {
+  let button = e.target;
+  button.classList.add('active');
+  let elem = document.querySelector("#b");
+  elem.classList.remove('active');
+  
+  let d = document.getElementById('d');
+  let f = document.getElementById('f');
+  let g = document.getElementById('g');
+  let h = document.getElementById('h');
+  let j = document.getElementById('j');
+  let k = document.getElementById('k');
+  let l = document.getElementById('l');
+  let r = document.getElementById('r');
+  let t = document.getElementById('t');
+  let u = document.getElementById('u');
+  let i = document.getElementById('i');
+  let o = document.getElementById('o');
+
+
+  d.innerText = "C";f.innerText = "D";g.innerText = "E";h.innerText = "F";j.innerText = "G";k.innerText = "A";
+  l.innerText = "B";r.innerText = "C#";t.innerText = "D#";u.innerText = "F#";i.innerText = "G#";o.innerText = "A#";
+  
+  note.addEventListener('ended',() =>{key.classList.remove('active');})
 }
